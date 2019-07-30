c64047de88046f3253a7b1688a087c1dfecc6bda
Simon Struck <simon@simonscode.org>
2019-07-31 00:11:11 +0200

    Remove error pseudo handling

--- Manager/src/main/java/org/simonscode/asyncfm/Manager.java
+++ Manager/src/main/java/org/simonscode/asyncfm/Manager.java
@@ -8,22 +8,18 @@ import java.io.FileInputStream;
 import java.io.IOException;
 
 public class Manager {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         if (args.length != 1) {
             System.err.println("Arguments: <input file>");
             return;
         }
 
-        try (FileInputStream fis = new FileInputStream(args[0])) {
-            StructUtils walker = new StructUtils(fis, true);
-            Node root = walker.readTree();
+        FileInputStream fis = new FileInputStream(args[0]);
+        StructUtils walker = new StructUtils(fis, true);
+        Node root = walker.readTree();
 
-            System.out.printf("%n%s Files loaded.", root.countChildren() + 1);
-            FileManager fileManager = new FileManager(root);
-            fileManager.createAndShowGui();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        FileManager fileManager = new FileManager(root);
+        fileManager.createAndShowGui();
     }
 }
